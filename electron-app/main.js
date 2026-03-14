6c33cdd33dacd49cfdcf0a270bd01a3e6f0df3a9
Guy Cochran <guycochran@gmail.com>
2026-03-14 00:38:48 +0000

    Cut 1.1.0-beta.8

--- electron-app/main.js
+++ electron-app/main.js
@@ -262,7 +262,7 @@ function createTray() {
         dialog.showMessageBox({
           type: 'info',
           title: 'About StreamVoice',
-          message: 'StreamVoice v1.1.0-beta.7',
+          message: 'StreamVoice v1.1.0-beta.8',
           detail: 'Professional voice control for OBS Studio.\n\nMade with ❤️ for streamers.',
           buttons: ['OK']
         });
